91625ac3264d5e415c475f8c266de34c9882c725
Tushar Kale <tushar@procmart.com>
2020-08-21 00:06:21 +0530

    Added modal to view map

--- frontend/src/shared/components/UIElements/Modal.js
+++ frontend/src/shared/components/UIElements/Modal.js
@@ -2,17 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './Modal.css'
 
+import Backdrop from './Backdrop'
+import { CSSTransition } from 'react-transition-group'
+
 const ModalOverlay = props => {
     const content = (
         <div className={ `modal ${props.className}` } style={ props.style }>
             <header className={ `modal__header ${props.headerClass}`}>
                 <h2>{props.header}</h2>
             </header>
-            <form 
-                onSubmit={
-                    props.onSubmit ? props.onSubmit : event => event.preventDefault
-                }
-            >
+            <form onSubmit={props.onSubmit ? props.onSubmit : event => event.preventDefault()}>
                 <div className={`modal__content ${props.contentClass}`}>
                     {props.children}
                 </div>
@@ -22,7 +21,7 @@ const ModalOverlay = props => {
             </form>
         </div>
     )
-    return ReacrDOM.createPortal(content, document.getElementById('modal-hook'));
+    return ReactDOM.createPortal(content, document.getElementById('modal-hook'));
 }
 
 const Modal = props => {
