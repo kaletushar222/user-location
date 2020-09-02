dae991ff1fa911eb9356512e17210bda66f73292
Tushar Kale <tushar@procmart.com>
2020-09-03 00:09:33 +0530

    UI fixes

--- frontend/src/places/components/PlaceList.js
+++ frontend/src/places/components/PlaceList.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import Card from '../../shared/components/UIElements/Card';
 import PlaceItem from './PlaceItem'
+import Button from '../../shared/components/FormElements/Button'
+
 import './PlaceList.css'
 
 const PlaceList = props => {
@@ -9,7 +11,7 @@ const PlaceList = props => {
         return <div className="place-list center">
             <Card>
                 <h2>No places found , Create new</h2>
-                <button>Share place</button>
+                <Button to="/places/new">Share place</Button>
             </Card>
         </div>
     }
