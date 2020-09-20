3cc6a95de983723d7a4aaa0d202f52b35add78aa
Tushar Kale <tushar@procmart.com>
2020-09-21 00:56:47 +0530

    Added user get, login and signup apis

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const placesControllers = require('../controllers/places-controller')
+const placesControllers = require('../controllers/places-controllers')
 
 router.get("/:placeid", placesControllers.getPlaceById);
 router.get("/user/:userId", placesControllers.getPlacesByUserId);
